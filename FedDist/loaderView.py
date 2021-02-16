67de6e116c3946c01f34a23db37042c5cc6684bf
Sannara Ek <sannara.ek@etu.univ-grenoble-alpes.fr>
2021-02-16 19:37:08 +0700

    third commit

--- FedDist/loaderView.py
+++ FedDist/loaderView.py
@@ -14,13 +14,9 @@ import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
 import time
-from tensorflow.python.client import device_lib
-import tensorflow as tf
 import os
-import hickle as hkl 
-import skbio as skb
+import hickle as hkl
 from scipy.spatial import distance_matrix
-
 
 
 # In[6]:
@@ -64,25 +60,27 @@ asyncTest = False
 
 
 if(dataSetName == 'UCI'):
-    ACTIVITY_LABEL = ['WALKING', 'WALKING_UPSTAIRS','WALKING_DOWNSTAIRS', 'SITTING', 'STANDING', 'LAYING']
+    ACTIVITY_LABEL = ['WALKING', 'WALKING_UPSTAIRS',
+                      'WALKING_DOWNSTAIRS', 'SITTING', 'STANDING', 'LAYING']
 else:
-    ACTIVITY_LABEL = ['climbingdown', 'climbingup', 'jumping','lying', 'running', 'sitting', 'standing', 'walking']
+    ACTIVITY_LABEL = ['climbingdown', 'climbingup', 'jumping',
+                      'lying', 'running', 'sitting', 'standing', 'walking']
 activityCount = len(ACTIVITY_LABEL)
 
 if(modelType == "DNN"):
-    architectureType = str(algorithm)+'_'+str(learningRate)+'LR_'+str(localEpoch)+'LE_'+str(communicationRound)+'CR_400D_100D_'+str(dataConfig)+'_'+str(optimizer)+'_MULTI_GRAPH'
-else: 
-    architectureType = str(algorithm)+'_'+str(learningRate)+'LR_'+str(localEpoch)+'LE_'+str(communicationRound)+'CR_196-16C_4M_1024D_'+str(dataConfig)+'_'+str(optimizer)+'_MULTI_GRAPH'
+    architectureType = str(algorithm)+'_'+str(learningRate)+'LR_'+str(localEpoch)+'LE_'+str(
+        communicationRound)+'CR_400D_100D_'+str(dataConfig)+'_'+str(optimizer)+'_MULTI_GRAPH'
+else:
+    architectureType = str(algorithm)+'_'+str(learningRate)+'LR_'+str(localEpoch)+'LE_'+str(
+        communicationRound)+'CR_196-16C_4M_1024D_'+str(dataConfig)+'_'+str(optimizer)+'_MULTI_GRAPH'
 mainDir = ''
 filepath = mainDir + 'savedModels/'+architectureType+'/'+dataSetName+'/'
 os.makedirs(filepath, exist_ok=True)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-if(dataSetName=='UCI'):
+if(dataSetName == 'UCI'):
     clientCount = 5
 else:
     clientCount = 15
-print("Num GPUs Available: ", len(
-tf.config.experimental.list_physical_devices('GPU')))
 
 
 # In[4]:
@@ -91,33 +89,39 @@ tf.config.experimental.list_physical_devices('GPU')))
 # filepath = mainDir + 'savedModels/'+architectureType+'/'+dataSetName+'/'
 os.makedirs(filepath+'trainingStats', exist_ok=True)
 
-trainLossHistory = hkl.load("trainingStats/trainLossHistory.hkl" )
-trainAccHistory = hkl.load( "trainingStats/trainAccHistory.hkl" )
-stdTrainLossHistory = hkl.load( "trainingStats/stdTrainLossHistory.hkl" )
-stdTrainAccHistory = hkl.load( "trainingStats/stdTrainAccHistory.hkl" )
+trainLossHistory = hkl.load("trainingStats/trainLossHistory.hkl")
+trainAccHistory = hkl.load("trainingStats/trainAccHistory.hkl")
+stdTrainLossHistory = hkl.load("trainingStats/stdTrainLossHistory.hkl")
+stdTrainAccHistory = hkl.load("trainingStats/stdTrainAccHistory.hkl")
 
-testLossHistory = hkl.load( "trainingStats/testLossHistory.hkl" )
-testAccHistory = hkl.load( "trainingStats/testAccHistory.hkl" )
-stdTestLossHistory = hkl.load( "trainingStats/stdTestLossHistory.hkl" )
-stdTestAccHistory = hkl.load( "trainingStats/stdTestAccHistory.hkl" )
+testLossHistory = hkl.load("trainingStats/testLossHistory.hkl")
+testAccHistory = hkl.load("trainingStats/testAccHistory.hkl")
+stdTestLossHistory = hkl.load("trainingStats/stdTestLossHistory.hkl")
+stdTestAccHistory = hkl.load("trainingStats/stdTestAccHistory.hkl")
 
 
 if(fastMode == False):
-    clientStdTrainLossHistory = hkl.load( "trainingStats/clientStdTrainLossHistory.hkl" )
-    clientStdTrainAccHistory = hkl.load( "trainingStats/clientStdTrainAccHistory.hkl" )
-    clientStdTestLossHistory = hkl.load( "trainingStats/clientStdTestLossHistory.hkl" )
-    clientStdTestAccHistory = hkl.load( "trainingStats/clientStdTestAccHistory.hkl" )
+    clientStdTrainLossHistory = hkl.load(
+        "trainingStats/clientStdTrainLossHistory.hkl")
+    clientStdTrainAccHistory = hkl.load(
+        "trainingStats/clientStdTrainAccHistory.hkl")
+    clientStdTestLossHistory = hkl.load(
+        "trainingStats/clientStdTestLossHistory.hkl")
+    clientStdTestAccHistory = hkl.load(
+        "trainingStats/clientStdTestAccHistory.hkl")
 
-    clientTrainLossHistory = hkl.load( "trainingStats/clientTrainLossHistory.hkl" )
-    clientTrainAccHistory = hkl.load( "trainingStats/clientTrainAccHistory.hkl" )
-    clientTestLossHistory = hkl.load( "trainingStats/clientTestLossHistory.hkl" )
-    clientTestAccHistory = hkl.load( "trainingStats/clientTestAccHistory.hkl" )
+    clientTrainLossHistory = hkl.load(
+        "trainingStats/clientTrainLossHistory.hkl")
+    clientTrainAccHistory = hkl.load("trainingStats/clientTrainAccHistory.hkl")
+    clientTestLossHistory = hkl.load("trainingStats/clientTestLossHistory.hkl")
+    clientTestAccHistory = hkl.load("trainingStats/clientTestAccHistory.hkl")
 
 if(algorithm != 'FEDPER'):
-    serverTrainLossHistory = hkl.load( "trainingStats/serverTrainLossHistory.hkl" )
-    serverTrainAccHistory = hkl.load( "trainingStats/serverTrainAccHistory.hkl" )
-    serverTestLossHistory = hkl.load( "trainingStats/serverTestLossHistory.hkl" )
-    serverTestAccHistory = hkl.load( "trainingStats/serverTestAccHistory.hkl" )
+    serverTrainLossHistory = hkl.load(
+        "trainingStats/serverTrainLossHistory.hkl")
+    serverTrainAccHistory = hkl.load("trainingStats/serverTrainAccHistory.hkl")
+    serverTestLossHistory = hkl.load("trainingStats/serverTestLossHistory.hkl")
+    serverTestAccHistory = hkl.load("trainingStats/serverTestAccHistory.hkl")
 
 
 # In[10]:
@@ -127,30 +131,37 @@ epoch_range = range(1, communicationRound+1)
 
 
 if(algorithm != "FEDPER"):
-    plt.plot(epoch_range, serverTrainAccHistory, label = 'Server Train')
-    plt.plot(epoch_range, serverTestAccHistory, label= 'Server Test')
-    plt.plot(epoch_range, serverTrainAccHistory,markevery=[np.argmax(serverTrainAccHistory)], ls="", marker="o",color="blue")
-    plt.plot(epoch_range, serverTestAccHistory,markevery=[np.argmax(serverTestAccHistory)], ls="", marker="o",color="orange") 
+    plt.plot(epoch_range, serverTrainAccHistory, label='Server Train')
+    plt.plot(epoch_range, serverTestAccHistory, label='Server Test')
+    plt.plot(epoch_range, serverTrainAccHistory, markevery=[
+             np.argmax(serverTrainAccHistory)], ls="", marker="o", color="blue")
+    plt.plot(epoch_range, serverTestAccHistory, markevery=[
+             np.argmax(serverTestAccHistory)], ls="", marker="o", color="orange")
 
-plt.errorbar(epoch_range, trainAccHistory, yerr=stdTrainAccHistory, label='Client Own Train',alpha=0.6)
-plt.errorbar(epoch_range, testAccHistory, yerr=stdTestAccHistory, label='Client Own Test',alpha=0.6)
+plt.errorbar(epoch_range, trainAccHistory, yerr=stdTrainAccHistory,
+             label='Client Own Train', alpha=0.6)
+plt.errorbar(epoch_range, testAccHistory, yerr=stdTestAccHistory,
+             label='Client Own Test', alpha=0.6)
 
 if(asyncTest):
-    plt.axvline(roundEnd[0], 0, 1,color ="blue")
-    plt.axvline(roundEnd[1], 0, 1,color ="blue")
+    plt.axvline(roundEnd[0], 0, 1, color="blue")
+    plt.axvline(roundEnd[1], 0, 1, color="blue")
 
 if(fastMode == False):
-    plt.errorbar(epoch_range, clientTrainAccHistory, yerr=clientStdTrainAccHistory, label='Client All Train',alpha=0.6)
-    plt.errorbar(epoch_range, clientTestAccHistory, yerr=clientStdTestAccHistory, label='Client All Test',alpha=0.6)
-    plt.plot(epoch_range, clientTrainAccHistory,markevery=[np.argmax(clientTrainAccHistory)], ls="", marker="o",color="purple")
-    plt.plot(epoch_range, clientTestAccHistory,markevery=[np.argmax(clientTestAccHistory)], ls="", marker="o",color="brown")  
+    plt.errorbar(epoch_range, clientTrainAccHistory,
+                 yerr=clientStdTrainAccHistory, label='Client All Train', alpha=0.6)
+    plt.errorbar(epoch_range, clientTestAccHistory,
+                 yerr=clientStdTestAccHistory, label='Client All Test', alpha=0.6)
+    plt.plot(epoch_range, clientTrainAccHistory, markevery=[
+             np.argmax(clientTrainAccHistory)], ls="", marker="o", color="purple")
+    plt.plot(epoch_range, clientTestAccHistory, markevery=[
+             np.argmax(clientTestAccHistory)], ls="", marker="o", color="brown")
 
 
-
-plt.plot(epoch_range, trainAccHistory,markevery=[np.argmax(trainAccHistory)], ls="", marker="o",color="green")
-plt.plot(epoch_range, testAccHistory,markevery=[np.argmax(testAccHistory)], ls="", marker="o",color="red")  
-
-
+plt.plot(epoch_range, trainAccHistory, markevery=[
+         np.argmax(trainAccHistory)], ls="", marker="o", color="green")
+plt.plot(epoch_range, testAccHistory, markevery=[
+         np.argmax(testAccHistory)], ls="", marker="o", color="red")
 
 
 plt.title('Model accuracy')
@@ -161,34 +172,43 @@ plt.savefig('LearningAccuracy.png', dpi=100)
 plt.show()
 
 if(algorithm != "FEDPER"):
-    plt.plot(epoch_range, serverTrainLossHistory, label = 'Server Train')
-    plt.plot(epoch_range, serverTestLossHistory, label= 'Server Test')
-    plt.plot(epoch_range, serverTrainLossHistory,markevery=[np.argmin(serverTrainLossHistory)], ls="", marker="o",color="blue")
-    plt.plot(epoch_range, serverTestLossHistory,markevery=[np.argmin(serverTestLossHistory)], ls="", marker="o",color="orange") 
+    plt.plot(epoch_range, serverTrainLossHistory, label='Server Train')
+    plt.plot(epoch_range, serverTestLossHistory, label='Server Test')
+    plt.plot(epoch_range, serverTrainLossHistory, markevery=[
+             np.argmin(serverTrainLossHistory)], ls="", marker="o", color="blue")
+    plt.plot(epoch_range, serverTestLossHistory, markevery=[
+             np.argmin(serverTestLossHistory)], ls="", marker="o", color="orange")
 
-plt.errorbar(epoch_range, trainLossHistory, yerr=stdTrainLossHistory, label='Client Own Train',alpha=0.6)
-plt.errorbar(epoch_range, testLossHistory, yerr=stdTestLossHistory, label='Client Own Test',alpha=0.6)
+plt.errorbar(epoch_range, trainLossHistory,
+             yerr=stdTrainLossHistory, label='Client Own Train', alpha=0.6)
+plt.errorbar(epoch_range, testLossHistory, yerr=stdTestLossHistory,
+             label='Client Own Test', alpha=0.6)
 
 if(fastMode == False):
-    plt.errorbar(epoch_range, clientTrainLossHistory, yerr=clientStdTrainLossHistory, label='Client All Train',alpha=0.6)
-    plt.errorbar(epoch_range, clientTestLossHistory, yerr=clientStdTestLossHistory, label='Client All Test',alpha=0.6)
-    plt.plot(epoch_range, clientTrainLossHistory,markevery=[np.argmin(clientTrainLossHistory)], ls="", marker="o",color="purple")
-    plt.plot(epoch_range, clientTestLossHistory,markevery=[np.argmin(clientTestLossHistory)], ls="", marker="o",color="brown")  
+    plt.errorbar(epoch_range, clientTrainLossHistory,
+                 yerr=clientStdTrainLossHistory, label='Client All Train', alpha=0.6)
+    plt.errorbar(epoch_range, clientTestLossHistory,
+                 yerr=clientStdTestLossHistory, label='Client All Test', alpha=0.6)
+    plt.plot(epoch_range, clientTrainLossHistory, markevery=[
+             np.argmin(clientTrainLossHistory)], ls="", marker="o", color="purple")
+    plt.plot(epoch_range, clientTestLossHistory, markevery=[
+             np.argmin(clientTestLossHistory)], ls="", marker="o", color="brown")
 
 
-
-plt.plot(epoch_range, trainLossHistory,markevery=[np.argmin(trainLossHistory)], ls="", marker="o",color="green")
-plt.plot(epoch_range, testLossHistory,markevery=[np.argmin(testLossHistory)], ls="", marker="o",color="red")  
+plt.plot(epoch_range, trainLossHistory, markevery=[
+         np.argmin(trainLossHistory)], ls="", marker="o", color="green")
+plt.plot(epoch_range, testLossHistory, markevery=[
+         np.argmin(testLossHistory)], ls="", marker="o", color="red")
 
 
 if(asyncTest):
-    plt.axvline(roundEnd[0], 0, 1,color ="blue")
-    plt.axvline(roundEnd[1], 0, 1,color ="blue")
+    plt.axvline(roundEnd[0], 0, 1, color="blue")
+    plt.axvline(roundEnd[1], 0, 1, color="blue")
 
 plt.title('Model Loss')
 plt.ylabel('Loss')
 plt.xlabel('Communication Round')
-plt.legend(loc= 'upper right')
+plt.legend(loc='upper right')
 plt.savefig('LearningLoss.png', dpi=100)
 plt.show()
 
@@ -224,7 +244,7 @@ plt.show()
 #     plt.legend(['Client Train','Client Test',], loc='lower right')
 # else:
 #     plt.legend(['Client Train','Client Test'], loc='lower right')
-    
+
 # plt.savefig('LearningAccuracyClientSingle.png', dpi=100)
 # plt.clf()
 
@@ -302,7 +322,3 @@ plt.show()
 
 
 # In[ ]:
-
-
-
-
